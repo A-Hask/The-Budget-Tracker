7edd3ba145f304fa44fc939dd4a3f151b2b83662
Qu33nArlene <ahaskins93@yahoo.com>
2022-08-05 18:09:07 -0400

    added manifest.json, service-worker.js, and .gitignore

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  "/public/index.html",
+  "/public/css/styles.css",
+  "/public/js/index.js",
+  "/routes/api.js",
+  "/models/transaction.js",
+];
