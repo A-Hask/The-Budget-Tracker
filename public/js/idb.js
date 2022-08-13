90c61d6824b86d873567469500e19ef903fcabfa
Qu33nArlene <ahaskins93@yahoo.com>
2022-08-13 11:11:29 -0400

    added idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,10 +9,53 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
   db = event.target.result;
   if (navigator.onLine) {
-    //NEED CODE
+    uploadBudget();
   }
 };
 
 request.onerror = function (event) {
-  //NEED CODE
+  console.log(event.target.errorCode);
 };
+
+function saveBudget(record) {
+  const transaction = db.transaction(["new_budget", "readwrite"]);
+  const budgetObjectStore = transaction.objectStore("new_budget");
+
+  budgetObjectStore.add(record);
+  console.log("New budget saved!");
+}
+
+function uploadBudget() {
+  const transaction = db.transaction(["new_budget"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_budget");
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_budget"], "readwrite");
+          const budgetObjectStore = transaction.objectStore("new_budget");
+          budgetObjectStore.clear();
+
+          alert("All budget changes have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadBudget);
